2df53eeede751ea547a2b4f3e929551f9cbdac4d
monika <mstempniak@gmail.com>
2018-03-13 17:38:39 +0100

    add webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+var path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  devtool: 'cheap-eval-source-map',
+  entry: {
+    main: path.join(__dirname, 'src/js', 'app.jsx')
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].bundle.js'
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015', 'stage-2', 'react']
+          }
+        }
+      },
+      {
+        test: /\.(png|jpe?g|svg|gif|woff|otf)$/,
+        use: {loader: 'file-loader'}
+      },
+      {
+        test: /\.css/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+         fallback: "style-loader",
+         use: ['css-loader', 'sass-loader']
+       })
+     }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'index.html'),
+      hash: true
+    }),
+    new ExtractTextPlugin('style.css')
+  ]
+}
