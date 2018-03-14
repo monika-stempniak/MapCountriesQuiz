c34105dd5918d74dd8a92ad8f640c62535c8f6cc
monika <mstempniak@gmail.com>
2018-03-14 11:13:44 +0100

    add topbar component to app.jsx

--- src/js/components/app.jsx
+++ src/js/components/app.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Map } from './map.jsx';
+import { Topbar } from './topbar.jsx';
 
 class App extends React.Component {
   render() {
     return (
-      <Map />
+      <div>
+        <Topbar />
+        <Map />
+      </div>
     )
   }
 }
