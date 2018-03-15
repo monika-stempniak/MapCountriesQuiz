c9636a473d13c726e635b2cfe9ebc672133ae17d
monika <mstempniak@gmail.com>
2018-03-15 11:06:18 +0100

    add mouse events when hover over countries

--- src/js/components/map.jsx
+++ src/js/components/map.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-
-
 class Map extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      mapSvgStyle: {transform: `scale(1) translateY(0px)`}
+      mapSvgStyle: {transform: `scale(1) translateY(0px)`},
+      countryHover: {display: 'none'},
+      countryId: ''
     }
 }
 
@@ -23,8 +23,20 @@ class Map extends React.Component {
     })
   }
 
-  handleMouseEnter() {
+  handleMouseEnter(id) {
+    console.log("fgf",id)
+    this.setState({
+      countryHover: {display: 'block'},
+      countryId: id
+    })
 
+  }
+
+  handleMouseOut() {
+    this.setState({
+      countryHover: {display: 'none'},
+      countryId: ''
+    })
   }
 
   handleClickInPath(idCountry) {
@@ -41,17 +53,33 @@ class Map extends React.Component {
       title={elem.title}
       className="land"
       onClick={e => this.handleClickInPath(elem.id)}
-      d={elem.d}/>
-  
+      onMouseEnter={(e) => this.handleMouseEnter(elem.id)}
+      onMouseOut={() => this.handleMouseOut()}
+      d={elem.d}/>;
     })
+
+    console.log("id",this.state.countryId);
+
+    let country = [];
+    country = this.props.countries.filter( elem => {
+      return this.state.countryId === elem.id
+    })
+    let title = "";
+    if(country.length > 0 ) {
+      title = country[0].title
+
+      console.log(title);
+    }
+
 
     return (
       <section className="map">
         <figure className="map-container">
           <button className="btn btn-further" aria-hidden="true" onClick={() => this.handleClickFurther()}><i className="fas fa-minus"></i></button>
           <button className="btn btn-closer" aria-hidden="true" onClick={() => this.handleClickCloser()}><i className="fas fa-plus"></i></button>
+          <div className="country-name" style={this.state.countryHover}>{title}</div>
           <h2 className="map-title visuallyhidden">World map</h2>
-          <svg xmlns="http://www.w3.org/2000/svg" className="map-svg" viewBox="0 0 1009.12 665.24" onMouseEnter={() => this.handleMouseEnter()} style={this.state.mapSvgStyle}>
+          <svg xmlns="http://www.w3.org/2000/svg" className="map-svg" viewBox="0 0 1009.12 665.24" style={this.state.mapSvgStyle}>
             <g>
               {list}
             </g>
