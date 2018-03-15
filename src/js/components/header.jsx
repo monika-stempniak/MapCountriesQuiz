da1ef119663871d4b8a2b7a982fe268f776b6b59
monika <mstempniak@gmail.com>
2018-03-15 20:43:54 +0100

    add header component

---
+++ src/js/components/header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header className="topbar-header">
+        <h1 className="header-title">Map Countries Quiz</h1>
+      </header>
+    )
+  }
+}
+
+export { Header };
+
