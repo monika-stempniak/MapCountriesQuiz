3acb6d4b95d5158b6dd117d99498fb682a7b2de2
Monika Stempniak <>
2018-08-19 17:49:32 +0200

    Create Home component

---
+++ src/js/scenes/Home.jsx
@@ -0,0 +1,14 @@
+// @flow
+import React from 'react'
+
+const Home = () => {
+  return (
+    <section className="home">
+      <h1 className="home-title">
+        Welcome
+      </h1>
+    </section>
+  )
+}
+
+export default Home
