75a1bbc01de8ccb07ca75f1819143fbc57f102e7
Monika Stempniak <>
2018-08-19 17:50:09 +0200

    Create Results component

---
+++ src/js/scenes/Results.jsx
@@ -0,0 +1,14 @@
+// @flow
+import React from 'react'
+
+const Results = () => {
+  return (
+    <section className="results">
+      <h1 className="results-title">
+        Results
+      </h1>
+    </section>
+  )
+}
+
+export default Results
