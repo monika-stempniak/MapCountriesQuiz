d56b38c742f1bb26ee13fd25df940ce360d70f2d
Monika Stempniak <>
2018-08-19 17:51:46 +0200

    Replace App component name with Quiz, create new App component

--- src/js/scenes/App.jsx
+++ src/js/scenes/App.jsx
@@ -1,45 +1,6 @@
-// @flow
-import * as React from 'react'
-import Map from './Map'
-import Topbar  from './Topbar'
-import countries from '../auxiliaryArrays/countries'
+import React from 'react'
+import Routing from './Routing'
 
-type State = {
-  userAnswer: string,
-}
-
-type Props = {}
-
-// type Country = {id: string, title: string, d: string}
-
-class App extends React.Component<Props, State> {
-  state = {
-    userAnswer: '',
-  }
-
-  handleClick = (countryId: string) => {
-    this.setState({
-      userAnswer: countryId,
-    })
-  }
-
-  // prepareListOfCountriesId = (countries: Array<Country>) : Array<string>  => {
-  //   return countries.map(country => country.id)
-  // }
-
-  render() {
-    const { userAnswer } = this.state
-
-    return (
-      <div>
-        <Topbar
-          answer={userAnswer}
-          // listId={this.prepareListOfCountriesId(countries)}
-        />
-        <Map clickMap={this.handleClick} countries={countries} />
-      </div>
-    )
-  }
-}
+const App = () => <Routing />
 
 export default App
---
+++ src/js/scenes/Quiz.jsx
@@ -0,0 +1,45 @@
+// @flow
+import * as React from 'react'
+import Map from './Map'
+import Topbar  from './Topbar'
+import countries from '../auxiliaryArrays/countries'
+
+type State = {
+  userAnswer: string,
+}
+
+type Props = {}
+
+// type Country = {id: string, title: string, d: string}
+
+class Quiz extends React.Component<Props, State> {
+  state = {
+    userAnswer: '',
+  }
+
+  handleClick = (countryId: string) => {
+    this.setState({
+      userAnswer: countryId,
+    })
+  }
+
+  // prepareListOfCountriesId = (countries: Array<Country>) : Array<string>  => {
+  //   return countries.map(country => country.id)
+  // }
+
+  render() {
+    const { userAnswer } = this.state
+
+    return (
+      <div>
+        <Topbar
+          answer={userAnswer}
+          // listId={this.prepareListOfCountriesId(countries)}
+        />
+        <Map clickMap={this.handleClick} countries={countries} />
+      </div>
+    )
+  }
+}
+
+export default Quiz
