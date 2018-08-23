37b8f475d449fc944cbcd20bc0cc785aa54aaac4
Monika Stempniak <>
2018-08-23 17:03:56 +0200

    Add redirect and disable functionality to Routing component

--- src/js/scenes/Routing.jsx
+++ src/js/scenes/Routing.jsx
@@ -1,34 +1,63 @@
+// @flow
 import React from 'react'
 import {
   BrowserRouter as Router,
   Route,
   NavLink,
   Switch,
+  Redirect,
 } from 'react-router-dom'
+import { connect } from 'react-redux'
 import Home from './Home'
 import Quiz from './Quiz'
 import Results from './Results'
 import AppWrapper from '../HOC/AppWrapper'
+import addUserName from '../actions/userAction'
 
-const Links = () => (
-  <nav className="links">
-    <NavLink className="links-link" exact activeClassName="active" to="/">Home</NavLink>
-    <NavLink className="links-link" to="/quiz">Quiz</NavLink>
-    <NavLink className="links-link" to="/results">Results</NavLink>
-  </nav>
-)
 
-const Routing = () => (
-  <Router>
-    <div className="routing">
-      <Links />
-      <Switch>
-        <Route exact path="/" component={AppWrapper(Home)} />
-        <Route path="/quiz" component={Quiz} />
-        <Route path="/results" component={AppWrapper(Results)} />
-      </Switch>
-    </div>
-  </Router>
-)
+type Props = {
+  userName: string;
+}
 
-export default Routing
+const Routing = (props: Props) => {
+
+  const { userName } = props
+  const disabledClass = userName ? '' : 'links-link--disabled'
+
+  return (
+    <Router>
+      <div className="routing">
+        <nav className="links">
+          <NavLink className="links-link" exact activeClassName="active" to="/">Home</NavLink>
+          <NavLink className={`links-link ${disabledClass}`} to="/quiz">Quiz</NavLink>
+          <NavLink className={`links-link ${disabledClass}`} to="/results">Results</NavLink>
+        </nav>
+        <Switch>
+          <Route exact path="/" component={AppWrapper(Home)} />
+          <Route
+            path="/quiz"
+            render={() => (
+              userName
+              ? <Quiz />
+              : <Redirect to="/" />
+            )}
+          />
+          <Route
+            path="/results"
+            render={() => (
+              userName
+              ? <Results />
+              : <Redirect to="/" />
+            )}
+          />
+        </Switch>
+      </div>
+    </Router>
+  )
+}
+
+const mapStateToProps = state => ({
+  userName: state.user.name,
+})
+
+export default connect(mapStateToProps, { addUserName })(Routing)
