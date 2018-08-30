26bbea9daf9406d752d48f32deca0264e1b2ce33
Monika Stempniak <>
2018-08-31 00:26:10 +0200

    Rebuild Results

--- src/js/scenes/Results.jsx
+++ src/js/scenes/Results.jsx
@@ -4,36 +4,37 @@ import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
 // import AppWrapper from '../HOC/AppWrapper'
 import Button from '../components/Button'
-import { addUserName, addUserResults } from      '../actions/userAction'
+import { addUserName, fetchCountries, addUserAnswers } from '../actions/userAction'
 
-type Result = {
-  id: number,
-  country: string,
-  code: string,
-  flag: string,
-  capital: string,
-  answer: string,
-}
+// type Result = {
+//   id: number,
+//   country: string,
+//   code: string,
+//   flag: string,
+//   capital: string,
+//   answer: string,
+// }
 
 type Props = {
   userName: string,
-  results: Array<Result>,
+  countries: Array<any>,
+  userAnswers: Array<any>,
   history: Array<string>,
 }
 
-const Results = ({ userName, results, history }: Props) => {
+const Results = ({ userName, countries, userAnswers, history }: Props) => {
 
   const prepareResults = () => {
-    return results && results.map(result => (
-      <tr key={result.id}>
-        <td>{result.id}</td>
-        <td>{result.country}</td>
-        <td>{result.code}</td>
-        <td>{result.capital}</td>
+    return countries && countries.map((country, i) => (
+      <tr key={country.alpha2Code}>
+        <td>{i}</td>
+        <td>{country.name}</td>
+        <td>{country.alpha2Code}</td>
+        <td>{country.capital}</td>
         <td>
-          <img src={result.flag} alt={`${result.country}'s flag`} width='70' />
+          <img src={country.flag} alt={`${country.country}'s flag`} width='70' />
         </td>
-        <td>{result.answer}</td>
+        <td>{userAnswers[i].answer}</td>
       </tr>
     ))
   }
@@ -77,7 +78,8 @@ const Results = ({ userName, results, history }: Props) => {
 
 const mapStateToProps = state => ({
   userName: state.user.name,
-  results: state.user.results,
+  countries: state.user.countries,
+  userAnswers: state.user.answers,
 })
 
-export default connect(mapStateToProps, { addUserName, addUserResults })(withRouter(Results))
+export default connect(mapStateToProps, { addUserName, fetchCountries, addUserAnswers })(withRouter(Results))
