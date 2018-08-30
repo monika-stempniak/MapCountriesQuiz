6cd2973b88c392763fefd966f2950a16a66c64c6
Monika Stempniak <>
2018-08-31 00:20:00 +0200

    Add fetchCountries function, rename addUserResults for addUserAnswers, create setCountries additional function

--- src/js/actions/userAction.js
+++ src/js/actions/userAction.js
@@ -1,4 +1,6 @@
-import { USER_NAME, USER_RESULTS } from './types'
+import { USER_NAME, USER_ANSWERS, FETCH_COUNTRIES } from './types'
+import getRandomNumber from '../helpers/getRandomNumber'
+// import mock from './responseMock';
 
 export const addUserName = (name) => dispatch => (
   dispatch({
@@ -7,9 +9,51 @@ export const addUserName = (name) => dispatch => (
   })
 )
 
-export const addUserResults = (results) => dispatch => {
+export const addUserAnswers = (answers) => dispatch => {
   return dispatch({
-    type: USER_RESULTS,
-    payload: results,
+    type: USER_ANSWERS,
+    payload: answers,
   })
+}
+
+function setCountries(countries, dispatch) {
+  const excludedCountries = ['AQ', 'UM']
+  const arrayLength = 3
+  let arrayOfCountries = []
+  for(let i = 0; i < arrayLength; i++) {
+    let randomNumber = getRandomNumber(countries)
+    let randomCountry = countries[randomNumber]
+    if (excludedCountries.indexOf(randomCountry.alpha2Code) !== -1) {
+      randomNumber = getRandomNumber(countries)
+      randomCountry = countries[randomNumber]
+    }
+    if (arrayOfCountries.indexOf(randomCountry) !== -1) {
+      randomNumber = getRandomNumber(countries)
+      randomCountry = countries[randomNumber]
+    }
+    arrayOfCountries.push(countries[randomNumber])
+  }
+  dispatch({
+    type: FETCH_COUNTRIES,
+    payload: arrayOfCountries,
+  })
+}
+
+export const fetchCountries = () => dispatch => {
+  const url = 'https://restcountries.eu/rest/v2/all'
+
+  // setCountries(mock, dispatch);
+
+  fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.json()
+      } else {
+        throw new Error('A connection error has occurred!')
+      }
+    })
+    .then(countries => {
+      setCountries(countries, dispatch);
+    })
+    .catch(error => console.dir('Error: ', error)) // eslint-disable-line no-console
 }
