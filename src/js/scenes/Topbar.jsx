5e08aea29144a901dbbd5e360d7ba51e27230e23
Monika Stempniak <>
2018-09-01 23:28:37 +0200

    Extend Topbar component by new Header and update style

--- src/js/scenes/Topbar.jsx
+++ src/js/scenes/Topbar.jsx
@@ -1,11 +1,15 @@
 // @flow
-import React from 'react'
-import { connect } from 'react-redux'
-import { withRouter } from 'react-router-dom'
-import Header from '../components/Header'
-import Timer from '../components/Timer'
-import Button from '../components/Button'
-import { addUserName, addUserAnswers, fetchCountries } from '../actions/userAction'
+import React from "react";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
+import Header from "../components/Header";
+import Timer from "../components/Timer";
+import Button from "../components/Button";
+import {
+  addUserName,
+  addUserAnswers,
+  fetchCountries
+} from "../actions/userAction";
 
 type State = {
   countries: Array<any>,
@@ -13,7 +17,7 @@ type State = {
     code: string,
     name: string,
     flag: string,
-    capital: string,
+    capital: string
   },
   goodAnswer: number,
   badAnswer: number,
@@ -21,136 +25,149 @@ type State = {
   isHintLinkVisible: boolean,
   isHintVisible: boolean,
   hintMessage: string,
-  isTimeOut: boolean,
-}
+  isTimeOut: boolean
+};
 
 type Props = {
   answer: string,
   userName: string,
-  fetchCountries: () => () => ({
+  fetchCountries: () => () => {
     type: string,
-    payload: Array<any>,
-  }),
+    payload: Array<any>
+  },
   countries: Array<any>, //eslint-disable-line
   history: Array<string>,
-  addUserAnswers: ({ code: string, answer: boolean }) => () => ({
+  addUserAnswers: ({ code: string, answer: boolean }) => () => {
     type: string,
-    payload: Array<{ code: string, answer: boolean }>,
-  }),
-}
+    payload: Array<{
+      code: string,
+      answer: boolean
+    }>
+  }
+};
 
 class Topbar extends React.Component<Props, State> {
   state = {
     countries: [],
     country: {
-      code: '',
-      name: '',
-      flag: 'http://via.placeholder.com/150x80?text=COUNTRY+FLAG',
-      capital: '',
+      code: "",
+      name: "",
+      flag: "http://via.placeholder.com/150x80?text=COUNTRY+FLAG",
+      capital: ""
     },
     goodAnswer: 0,
     badAnswer: 0,
     isStartClicked: false,
     isHintLinkVisible: false,
     isHintVisible: false,
-    hintMessage: '',
-    isTimeOut: false,
-  }
+    hintMessage: "",
+    isTimeOut: false
+  };
 
   componentWillMount() {
-    this.props.fetchCountries()
+    this.props.fetchCountries();
   }
 
   getTimer = (timeOut: boolean) => {
-    this.setState({
-      isTimeOut: timeOut,
-    })
-  }
+    this.setState({ isTimeOut: timeOut });
+  };
 
   handleStopAndResetQuiz = () => {
-    this.props.history.push('/results')
+    this.props.history.push("/results");
     this.setState({
       countries: [],
       country: {
-        code: '',
-        name: '',
-        flag: 'http://via.placeholder.com/150x80?text=COUNTRY+FLAG',
-        capital: '',
+        code: "",
+        name: "",
+        flag: "http://via.placeholder.com/150x80?text=COUNTRY+FLAG",
+        capital: ""
       },
       goodAnswer: 0,
       badAnswer: 0,
       isStartClicked: false,
       isHintLinkVisible: false,
       isHintVisible: false,
-      hintMessage: '',
-      isTimeOut: false,
-    })
-  }
+      hintMessage: "",
+      isTimeOut: false
+    });
+  };
 
-  hintTimer = () => setTimeout(() => {
-    this.setState({
-      isHintVisible: false,
-    })
-  }, 1000)
+  hintTimer = () =>
+    setTimeout(() => {
+      this.setState({ isHintVisible: false });
+    }, 1000);
 
   handleClickHint = (e: SyntheticMouseEvent<HTMLAnchorElement>) => {
     e.preventDefault();
-    this.setState({
-      isHintVisible: true,
-    })
-    this.hintTimer()
-  }
+    this.setState({ isHintVisible: true });
+    this.hintTimer();
+  };
 
   handleClickStart = () => {
-    const { countries } = this.state
-    const { name, capital } = this.state.country
-    this.setState({
-      isStartClicked: true,
-      country: {
-        code: countries[0].alpha2Code,
-        name: countries[0].name,
-        flag: countries[0].flag,
-        capital: countries[0].capital,
+    const { countries } = this.state;
+    const { name, capital } = this.state.country;
+    this.setState(
+      {
+        isStartClicked: true,
+        country: {
+          code: countries[0].alpha2Code,
+          name: countries[0].name,
+          flag: countries[0].flag,
+          capital: countries[0].capital
+        },
+        isHintLinkVisible: true
       },
-      isHintLinkVisible: true,
-    }, () => {
-      this.setState({
-        hintMessage: capital ? `Capital: ${capital}` : `Country: ${name}`,
-      })
-    })
-  }
+      () => {
+        this.setState({
+          hintMessage: capital ? `Capital: ${capital}` : `Country: ${name}`
+        });
+      }
+    );
+  };
 
   handleDeleteCountry = () => {
     let countries = [...this.state.countries]; //eslint-disable-line
-    countries.shift()
-    this.setState({ countries })
-  }
+    countries.shift();
+    this.setState({ countries });
+  };
 
   UNSAFE_componentWillReceiveProps(nextProps: Props) {
-    const { isStartClicked, goodAnswer, badAnswer, countries, isTimeOut } = this.state
-    const { code } = this.state.country
-    const { answer } = this.props
-    if(nextProps.countries) {
+    const {
+      isStartClicked,
+      goodAnswer,
+      badAnswer,
+      countries,
+      isTimeOut
+    } = this.state;
+    const { code } = this.state.country;
+    const { answer } = this.props;
+    if (nextProps.countries) {
       this.setState({
-        countries: [...nextProps.countries],
-      })
+        countries: [...nextProps.countries]
+      });
     }
     if (nextProps.answer && isStartClicked) {
-      this.handleDeleteCountry()
-      const countriesArrayLength = countries.length - 1
+      this.handleDeleteCountry();
+      const countriesArrayLength = countries.length - 1;
       if (isTimeOut === true || countriesArrayLength === 0) {
-        this.handleStopAndResetQuiz()
+        this.handleStopAndResetQuiz();
       } else {
-        this.handleClickStart()
+        this.handleClickStart();
       }
       if (nextProps.answer === code && answer !== nextProps.answer) {
-        this.setState({
-          goodAnswer: goodAnswer + 1,
-        }, () => this.props.addUserAnswers({ code, answer: true }))
+        this.setState(
+          {
+            goodAnswer: goodAnswer + 1
+          },
+          () => this.props.addUserAnswers({ code, answer: true })
+        );
       } else {
-        this.setState({
-          badAnswer: badAnswer + 1,
-        },() => this.props.addUserAnswers({ code, answer: false }))
+        this.setState(
+          {
+            badAnswer: badAnswer + 1
+          },
+          () => this.props.addUserAnswers({ code, answer: false })
+        );
       }
     }
   }
@@ -163,83 +180,97 @@ class Topbar extends React.Component<Props, State> {
       hintMessage,
       isStartClicked,
       goodAnswer,
-      badAnswer,
-    } = this.state
+      badAnswer
+    } = this.state;
 
-    const { flag, name } = this.state.country
+    const { flag, name } = this.state.country;
 
-    const { userName } = this.props
+    const { userName } = this.props;
 
-    const hintLinkVisibility = isHintLinkVisible ? 'isVisible' : ''
-    const hintVisibility = isHintVisible ? 'isVisible' : ''
+    const hintLinkVisibility = isHintLinkVisible ? "is-visible" : "";
+    const hintVisibility = isHintVisible ? "is-visible" : "";
 
     return (
-      <section className="topbar">
-        <Header section="topbar">
-          Map Countries Quiz
-        </Header>
-        <div className="topbar-content">
-          <div className="col-3">
-            <h3 className="quiz-question">
-              <span className="quiz-user">{userName}, </span>
-              which country the flag belongs to?
-            </h3>
-            <div className="quiz-row">
+      <Header>
+        <section className="topbar">
+          <div className="topbar__content">
+            <div className="topbar__col-3">
+              <h3 className="topbar__quiz-question">
+                <span className="topbar__user">{userName},</span>
+                which country the flag belongs to?
+              </h3>
               <Button
-                btnClass={`btn-quiz-hint ${hintLinkVisibility}`}
-                handleClick={this.handleClickHint}
+                btnClass="topbar__button--start"
+                handleClick={this.handleClickStart}
               >
-                Get a hint?
-              </Button>
-              <Button btnClass="btn btn-start" handleClick={this.handleClickStart}>
                 Start
               </Button>
+              <div className="topbar__quiz-hint">
+                <Button
+                  btnClass={`topbar__button--hint ${hintLinkVisibility}`}
+                  handleClick={this.handleClickHint}
+                >
+                  Get a hint?
+                </Button>
+                <span className={`topbar__hint ${hintVisibility}`}>
+                  {hintMessage}
+                </span>
+              </div>
             </div>
-            <p className={`hint ${hintVisibility}`}>
-              {hintMessage}
-            </p>
+            <div className="topbar__col-3">
+              <img className="topbar__flag" src={flag} alt={`${name}'s flag`} />
+            </div>
+            <div className="topbar__col-3">
+              <table className="topbar__table">
+                <thead>
+                  <tr>
+                    <th scope="col" rowSpan="2">
+                      Timer
+                    </th>
+                    <th scope="col" rowSpan="2">
+                      Nr of countries
+                    </th>
+                    <th scope="col" colSpan="2">
+                      Answers
+                    </th>
+                  </tr>
+                  <tr>
+                    <th scope="col" className="topbar__table--good">
+                      Good
+                    </th>
+                    <th scope="col" className="topbar__table--bad">
+                      Bad
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>
+                      <Timer
+                        isStartClicked={isStartClicked}
+                        getTime={this.getTimer}
+                      />
+                    </td>
+                    <td>{`${countries.length}/3`}</td>
+                    <td className="good">{goodAnswer}</td>
+                    <td className="bad">{badAnswer}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
           </div>
-          <div className="col-3">
-            <img
-              className="flag"
-              src={flag}
-              alt={`${name}'s flag`}
-            />
-          </div>
-          <div className="col-3">
-            <table className="table-score">
-              <thead>
-                <tr>
-                  <th scope="col" rowSpan="2">Timer</th>
-                  <th scope="col" rowSpan="2">Nr of Countries</th>
-                  <th scope="col" colSpan="2">Answers</th>
-                </tr>
-                <tr>
-                  <th scope="col" className="good">Good</th>
-                  <th scope="col" className="bad">Bad</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>
-                    <Timer isStartClicked={isStartClicked} getTime={this.getTimer} />
-                  </td>
-                  <td>{`${countries.length}/3`}</td>
-                  <td className="good">{goodAnswer}</td>
-                  <td className="bad">{badAnswer}</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
-      </section>
-    )
+        </section>
+      </Header>
+    );
   }
 }
 
 const mapStateToProps = state => ({
   userName: state.user.name,
-  countries: state.user.countries,
-})
+  countries: state.user.countries
+});
 
-export default connect(mapStateToProps, { addUserName, addUserAnswers, fetchCountries })(withRouter(Topbar))
+export default connect(
+  mapStateToProps,
+  { addUserName, addUserAnswers, fetchCountries }
+)(withRouter(Topbar));
