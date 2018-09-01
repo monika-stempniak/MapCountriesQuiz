8eba46fbf3fb52bb410f40fe2545926d26145600
Monika Stempniak <>
2018-09-01 23:32:02 +0200

    Update Results component and extend by Header component

--- src/js/scenes/Results.jsx
+++ src/js/scenes/Results.jsx
@@ -1,10 +1,16 @@
 // @flow
-import React from 'react'
-import { connect } from 'react-redux'
-import { withRouter } from 'react-router-dom'
-// import AppWrapper from '../HOC/AppWrapper'
-import Button from '../components/Button'
-import { addUserName, fetchCountries, addUserAnswers } from '../actions/userAction'
+import React from "react";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
+import Scoresheet from "../components/Scoresheet";
+import Scores from "../components/Scores";
+import Button from "../components/Button";
+import Header from "../components/Header";
+import {
+  addUserName,
+  fetchCountries,
+  addUserAnswers
+} from "../actions/userAction";
 
 // type Result = {
 //   id: number,
@@ -19,67 +25,60 @@ type Props = {
   userName: string,
   countries: Array<any>,
   userAnswers: Array<any>,
-  history: Array<string>,
-}
+  history: Array<string>
+};
 
 const Results = ({ userName, countries, userAnswers, history }: Props) => {
-
   const prepareResults = () => {
-    return countries && countries.map((country, i) => (
-      <tr key={country.alpha2Code}>
-        <td>{i}</td>
-        <td>{country.name}</td>
-        <td>{country.alpha2Code}</td>
-        <td>{country.capital}</td>
-        <td>
-          <img src={country.flag} alt={`${country.country}'s flag`} width='70' />
-        </td>
-        <td>{userAnswers[i].answer}</td>
-      </tr>
-    ))
-  }
+    return (
+      <Scoresheet userName={userName}>
+        {countries &&
+          countries.map((country, i) => (
+            <Scores
+              key={country.alpha2Code}
+              ordinalNumber={i + 1}
+              country={country.name}
+              code={country.alpha2Code}
+              capital={country.capital}
+              flag={country.flag}
+              userAnswer={userAnswers[i].answer}
+            />
+          ))}
+      </Scoresheet>
+    );
+  };
 
   const handleClickPlayAgain = () => {
-    history.push('/quiz')
-  }
+    history.push("/quiz");
+  };
 
   return (
-    <div className="results-wrapper">
-      <section className="results">
-        <h1 className="results-title">Results</h1>
+    <div className="results">
+      <Header>
         <Button
-          btnClass="btn btn-play"
+          btnClass="results__button--play"
           handleClick={handleClickPlayAgain}
         >
           Play again
         </Button>
-        <div className="col-3">
-          <table className="table-results">
-            <caption>User: { userName }</caption>
-            <thead>
-              <tr>
-                <th>Id</th>
-                <th>Country</th>
-                <th>Code</th>
-                <th>Capital</th>
-                <th>Flag</th>
-                <th>Answer</th>
-              </tr>
-            </thead>
-            <tbody>
-              { prepareResults() }
-            </tbody>
-          </table>
-        </div>
+        <h2 className="results__header">Results</h2>
+      </Header>
+      <section className="results__content">
+        <hr />
+        <div className="container">{prepareResults()}</div>
+        <hr />
       </section>
     </div>
-  )
-}
+  );
+};
 
 const mapStateToProps = state => ({
   userName: state.user.name,
   countries: state.user.countries,
-  userAnswers: state.user.answers,
-})
+  userAnswers: state.user.answers
+});
 
-export default connect(mapStateToProps, { addUserName, fetchCountries, addUserAnswers })(withRouter(Results))
+export default connect(
+  mapStateToProps,
+  { addUserName, fetchCountries, addUserAnswers }
+)(withRouter(Results));
