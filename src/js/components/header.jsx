ca6654042e8894e47d78402f547070cbfd124674
Monika Stempniak <>
2018-09-01 23:26:59 +0200

    Extend Header component

--- src/js/components/header.jsx
+++ src/js/components/header.jsx
@@ -1,20 +1,19 @@
 // @flow
-import * as React from 'react'
+import * as React from "react";
 
 type Props = {
-  section: string,
   children: React.Node
-}
+};
 
-function Header(props: Props) {
-  const { section, children } = props
-  return (
-    <header className={`${section}-header`}>
-      <h1 className="header-title">
-        {children}
-      </h1>
-    </header>
-  )
-}
+const Header = (props: Props) => (
+  <section className="header__wrapper">
+    <div className="container">
+      <header className="header">
+        <h1 className="header__title">Map Countries Quiz</h1>
+      </header>
+      {props.children}
+    </div>
+  </section>
+);
 
-export default Header
+export default Header;
