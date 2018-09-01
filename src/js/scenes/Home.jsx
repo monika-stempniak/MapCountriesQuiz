e2e36c09f8439e212e545b95e052b28b2c464247
Monika Stempniak <>
2018-09-01 23:27:47 +0200

    Extend Home component by new Header and update style

--- src/js/scenes/Home.jsx
+++ src/js/scenes/Home.jsx
@@ -1,91 +1,94 @@
 // @flow
-import * as React from 'react';
-import { connect } from 'react-redux'
-import { withRouter } from 'react-router-dom'
-import Header from '../components/Header'
-import Button from '../components/Button'
-import { addUserName } from '../actions/userAction'
+import * as React from "react";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
+import Header from "../components/Header";
+import Button from "../components/Button";
+import { addUserName } from "../actions/userAction";
 
 type State = {
   name: string,
-  isDisable: boolean,
-}
+  isDisable: boolean
+};
 
 type Props = {
-  addUserName: (name: string) => () => ({
+  addUserName: (
+    name: string
+  ) => () => {
     type: string,
-    payload: string,
-  }),
-  history: Array<string>,
-}
+    payload: string
+  },
+  history: Array<string>
+};
 
 class Home extends React.Component<Props, State> {
   state = {
-    name: '',
-    isDisable: true,
-  }
+    name: "",
+    isDisable: true
+  };
 
   handleChange = (e: SyntheticInputEvent<HTMLInputElement>) => {
-    (e.target: HTMLInputElement)
+    (e.target: HTMLInputElement);
     this.setState({
       [e.target.name]: e.target.value,
-      isDisable: false,
-    })
-  }
+      isDisable: false
+    });
+  };
 
   handleSubmit = (e: SyntheticMouseEvent<HTMLButtonElement>) => {
-    e.preventDefault()
+    e.preventDefault();
 
-    const { name } = this.state
-    const { addUserName, history } = this.props
+    const { name } = this.state;
+    const { addUserName, history } = this.props;
 
-    addUserName(name)
+    addUserName(name);
 
-    history.push('/quiz')
-  }
+    history.push("/quiz");
+  };
 
   render() {
-    const { name, isDisable } = this.state
+    const { name, isDisable } = this.state;
 
-    const disabledClass = isDisable
-    ? 'btn-disabled'
-    : ''
+    const disabledClass = isDisable ? "btn-disabled" : "";
 
     return (
-      <section className="home">
-        <Header section="home">
-          Welcome to the Map Countries Quiz
+      <div className="home">
+        <Header>
+          <h2 className="home__header">Welcome</h2>
+          <h3 className="home__header">Please, enter your name to proceed</h3>
         </Header>
-        <br />
-        <h2>Please enter your name to proceed</h2>
-        <br />
-        <form onSubmit={this.handleSubmit}>
-          <div>
-            <label htmlFor="name">
-              <input
-                type="text"
-                id="name"
-                className="home-input"
-                name="name"
-                onChange={this.handleChange}
-                value={name}
-                placeholder="your name"
-              />
-            </label>
-            <br />
-            <br />
-            <Button
-              htmlType="submit"
-              btnClass={`btn btn-submit ${disabledClass}`}
-              isDisable={isDisable}
-            >
-              Submit
-            </Button>
-          </div>
-        </form>
-      </section>
-    )
+        <section className="home__content">
+          <dir className="container">
+            <form onSubmit={this.handleSubmit} className="home__form">
+              <div className="home__form-container">
+                <label htmlFor="name" className="home__form-label">
+                  <input
+                    type="text"
+                    id="name"
+                    className="home__form-input"
+                    name="name"
+                    onChange={this.handleChange}
+                    value={name}
+                    placeholder="your name"
+                  />
+                </label>
+                <Button
+                  htmlType="submit"
+                  btnClass={`btn btn-submit ${disabledClass}`}
+                  isDisable={isDisable}
+                >
+                  Submit
+                </Button>
+              </div>
+            </form>
+          </dir>
+        </section>
+      </div>
+    );
   }
 }
 
-export default connect(null, { addUserName })(withRouter(Home))
+export default connect(
+  null,
+  { addUserName }
+)(withRouter(Home));
