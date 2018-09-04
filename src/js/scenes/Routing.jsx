7705a290c24f832b7af03cfeab6676faba2952ad
monika-stempniak <mstempniak@gmail.com>
2018-09-04 20:19:37 +0200

    Refactor Routing component, update style & classes according to BEM

--- src/js/scenes/Routing.jsx
+++ src/js/scenes/Routing.jsx
@@ -1,63 +1,77 @@
 // @flow
-import React from 'react'
+import React from "react";
 import {
   BrowserRouter as Router,
   Route,
   NavLink,
   Switch,
   Redirect,
-} from 'react-router-dom'
-import { connect } from 'react-redux'
-import Home from './Home'
-import Quiz from './Quiz'
-import Results from './Results'
-import AppWrapper from '../HOC/AppWrapper'
-import addUserName from '../actions/userAction'
-
+} from "react-router-dom";
+import { connect } from "react-redux";
+import Home from "./Home";
+import Quiz from "./Quiz";
+import Results from "./Results";
+// import AppWrapper from "../HOC/AppWrapper";
+import addUserName from "../actions/userAction";
 
 type Props = {
-  userName: string;
-}
+  userName: string,
+};
 
 const Routing = (props: Props) => {
-
-  const { userName } = props
-  const disabledClass = userName ? '' : 'links-link--disabled'
+  const { userName } = props;
+  const disabledClass = userName ? "" : "disabled";
 
   return (
     <Router>
-      <div className="routing">
-        <nav className="links">
-          <NavLink className="links-link" exact activeClassName="active" to="/">Home</NavLink>
-          <NavLink className={`links-link ${disabledClass}`} to="/quiz">Quiz</NavLink>
-          <NavLink className={`links-link ${disabledClass}`} to="/results">Results</NavLink>
-        </nav>
-        <Switch>
-          <Route exact path="/" component={AppWrapper(Home)} />
-          <Route
-            path="/quiz"
-            render={() => (
-              userName
-              ? <Quiz />
-              : <Redirect to="/" />
-            )}
-          />
-          <Route
-            path="/results"
-            render={() => (
-              userName
-              ? <Results />
-              : <Redirect to="/" />
-            )}
-          />
-        </Switch>
+      <div className="app__container">
+        <div className="routing">
+          <nav className="routing__nav">
+            <div className="container routing__nav-container">
+              <NavLink
+                className="routing__nav-link"
+                exact
+                activeClassName="active"
+                to="/"
+              >
+                Home
+              </NavLink>
+              <NavLink
+                className={`routing__nav-link ${disabledClass}`}
+                to="/quiz"
+              >
+                Quiz
+              </NavLink>
+              <NavLink
+                className={`routing__nav-link ${disabledClass}`}
+                to="/results"
+              >
+                Results
+              </NavLink>
+            </div>
+          </nav>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route
+              path="/quiz"
+              render={() => (userName ? <Quiz /> : <Redirect to="/" />)}
+            />
+            <Route
+              path="/results"
+              render={() => (userName ? <Results /> : <Redirect to="/" />)}
+            />
+          </Switch>
+        </div>
       </div>
     </Router>
-  )
-}
+  );
+};
 
 const mapStateToProps = state => ({
   userName: state.user.name,
-})
+});
 
-export default connect(mapStateToProps, { addUserName })(Routing)
+export default connect(
+  mapStateToProps,
+  { addUserName }
+)(Routing);
