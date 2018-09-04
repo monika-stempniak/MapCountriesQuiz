3c480884b60358cca720d47518bf430a1ec3bcff
monika-stempniak <mstempniak@gmail.com>
2018-09-04 20:14:51 +0200

    Refactor map component, update style & classes according to BEM

--- src/js/components/MapSVG.jsx
+++ src/js/components/MapSVG.jsx
@@ -1,48 +1,48 @@
 // @flow
-import * as React from 'react'
-import { MapInteractionCSS } from 'react-map-interaction'
-import Icon from './Icon'
+import * as React from "react";
+import { MapInteractionCSS } from "react-map-interaction";
+import Icon from "./Icon";
 
 type Props = {
   svgPathList: Array<React$Element<any>>,
-}
+};
 
 class MapSVG extends React.Component<Props> {
   componentDidMount() {
-    const svgMap = this.map
-    const svgParent = svgMap && svgMap.parentElement
-    svgParent && svgParent.classList.add("map-svg-wrapper")
+    const svgMap = this.map;
+    const svgParent = svgMap && svgMap.parentElement;
+    svgParent && svgParent.classList.add("map__svg-wrapper");
   }
 
-  map: ?Element
+  map: ?Element;
 
   render() {
-    const { svgPathList } = this.props
-    const btnPlus = <Icon sign="plus" />
-    const btnMinus = <Icon sign="minus" />
+    const { svgPathList } = this.props;
+    const btnPlus = <Icon sign="plus" />;
+    const btnMinus = <Icon sign="minus" />;
     return (
       <MapInteractionCSS
         showControls
         plusBtnContents={btnPlus}
         minusBtnContents={btnMinus}
-        btnClass="btn-map"
-        controlsClass="map-buttons"
+        btnClass="map__button"
+        controlsClass="map__button-wrapper"
         minScale={1}
         maxScale={10}
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          className="map-svg"
+          className="map__svg"
           viewBox="0 0 1009.12 665.24"
-          ref={(node) => { this.map = node }}
+          ref={node => {
+            this.map = node;
+          }}
         >
-          <g>
-            {svgPathList}
-          </g>
+          <g>{svgPathList}</g>
         </svg>
       </MapInteractionCSS>
-    )
+    );
   }
 }
 
-export default MapSVG
+export default MapSVG;
--- src/js/scenes/Map.jsx
+++ src/js/scenes/Map.jsx
@@ -1,96 +1,92 @@
 // @flow
-import React from 'react'
-import MapSVG from '../components/MapSVG'
+import React from "react";
+import MapSVG from "../components/MapSVG";
 
-type Country = {id: string, title: string, d: string}
+type Country = { id: string, title: string, d: string };
 
 type State = {
   countryId: string,
-}
+};
 
 type Props = {
   clickMap: (countryId: string) => void,
   countries: Array<Country>,
-}
+};
 
-class Map extends React.Component<Props,State> {
+class Map extends React.Component<Props, State> {
   state = {
-    countryId: '',
-  }
+    countryId: "",
+  };
 
   handleMouseEnter = (id: string) => {
     this.setState({
       countryId: id,
-    })
-  }
+    });
+  };
 
   handleMouseOut = () => {
     this.setState({
-      countryId: '',
-    })
-  }
+      countryId: "",
+    });
+  };
 
   handleBlur = () => {
     this.setState({
-      countryId: '',
-    })
-  }
+      countryId: "",
+    });
+  };
 
   handleClickInPath = (countryId: string) => {
-    const { clickMap } = this.props
-    clickMap(countryId)
-  }
+    const { clickMap } = this.props;
+    clickMap(countryId);
+  };
 
   prepareSVGPathList = (): Array<React$Element<any>> => {
-    const { countries } = this.props
+    const { countries } = this.props;
     const list = countries.map(elem => {
       return (
         <path
           key={elem.id}
           id={elem.id}
           title={elem.title}
-          className="land"
+          className="map__country"
           onClick={() => this.handleClickInPath(elem.id)}
           onMouseEnter={() => this.handleMouseEnter(elem.id)}
           onMouseOut={this.handleMouseOut}
           onBlur={this.handleBlur}
           d={elem.d}
         />
-      )
-    })
-    return list
-  }
+      );
+    });
+    return list;
+  };
 
   prepareCountryTitle = (): string => {
-    const { countries } = this.props
-    const { countryId } = this.state
-    const country = countries.filter(elem => countryId === elem.id)
-    let title = ''
-    title = country.length > 0 ? country[0].title : ''
-    return title
-  }
+    const { countries } = this.props;
+    const { countryId } = this.state;
+    const country = countries.filter(elem => countryId === elem.id);
+    let title = "";
+    title = country.length > 0 ? country[0].title : "";
+    return title;
+  };
 
   render() {
-    const titleClass = this.prepareCountryTitle() !== '' ? 'isHovered' : ''
+    const titleClass = this.prepareCountryTitle() !== "" ? "is-hovered" : "";
 
     return (
       <section className="map">
-        <figure className="map-container">
-          <figcaption className="map-title visuallyhidden">
+        <figure className="map__container">
+          <figcaption className="map__title visuallyhidden">
             World map
           </figcaption>
-          <div
-            className={`country-name ${titleClass}`}
-          >
+          <div className={`map__country-name ${titleClass}`}>
             {this.prepareCountryTitle()}
           </div>
-          <MapSVG
-            svgPathList={this.prepareSVGPathList()}
-          />
+          <MapSVG svgPathList={this.prepareSVGPathList()} />
         </figure>
       </section>
-    )
+    );
   }
 }
 
-export default Map
+export default Map;
