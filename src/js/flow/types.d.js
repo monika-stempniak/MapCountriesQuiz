a8d5dcec811dc0c19ffd1d51d33a6a0ba52a4c2b
monika-stempniak <mstempniak@gmail.com>
2018-09-24 23:30:39 +0200

    Create flow types definition file

---
+++ src/js/flow/types.d.js
@@ -0,0 +1,14 @@
+// @flow
+export type Countries = {
+  flag: string,
+  name: string,
+  alpha2Code: string,
+  capital: string,
+  region: string,
+  subregion: string,
+};
+
+export type Answers = {
+  code: string,
+  answer: boolean,
+};
