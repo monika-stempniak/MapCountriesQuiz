9bd9775502fc5509cf4aa636915f4d3e01b96005
monika-stempniak <mstempniak@gmail.com>
2018-09-27 22:18:20 +0200

    Add favicon to the title and logo, create Logo component

--- src/js/components/header.jsx
+++ src/js/components/header.jsx
@@ -1,5 +1,6 @@
 // @flow
 import * as React from "react";
+import Logo from "./Logo";
 
 type Props = {
   children: React.Node,
@@ -9,7 +10,10 @@ const Header = (props: Props) => (
   <section className="header__wrapper">
     <div className="container">
       <header className="header">
-        <h1 className="header__title">Map Countries Quiz</h1>
+        <h1 className="header__title">
+          <Logo />
+          Map Countries Quiz
+        </h1>
       </header>
       {props.children}
     </div>
